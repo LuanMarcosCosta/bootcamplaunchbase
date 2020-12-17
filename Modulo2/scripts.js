9813ae2c472c70df1669685d2deebd32c96b49d6
Luãn Costa <luanmarcoscosta@outlook.com>
2020-12-16 21:51:21 -0300

    Correção no ".modal-overlay"

--- Modulo2/scripts.js
+++ Modulo2/scripts.js
@@ -1,12 +1,12 @@
-const modaloverlay = document.querySelector('modal_overlay');
+const modalOverlay = document.querySelector('.modal_overlay');
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener("click", function() {
-        modaloverlay.classList.add('active')
+        modalOverlay.classList.add('active')
     })
 }
 
 document.querySelector(".close_modal").addEventListener("click", function() {
-    modaloverlay.classList.remove('active')
+    modalOverlay.classList.remove('active')
 })
