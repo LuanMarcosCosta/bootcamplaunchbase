3e0c8849de4b97a11bbc263af105203dde23bdea
Luãn Costa <luanmarcoscosta@outlook.com>
2020-12-26 10:14:43 -0300

    Implementacoes Modulo03

--- Modulo3/public/scripts.js
+++ Modulo3/public/scripts.js
@@ -3,18 +3,13 @@ const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener("click", function() {
-        const blogId = card.getAttribute("id");
+        const videoId = card.getAttribute("id");
         modalOverlay.classList.add('active')
-        modalOverlay.classList.add('maximize')
-        modalOverlay.querySelector("iframe").src = `https://blog.rocketseat.com.br/${blogId}`
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
     })
 }
 
 document.querySelector(".close_modal").addEventListener("click", function() {
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector("iframe").src = ""
-})
-
-document.querySelector('.maximize_modal').addEventListener('click', function() {
-    modalOverlay.classList.remove('maximize')
 })
