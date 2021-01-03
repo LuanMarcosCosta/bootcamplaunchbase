f631437ecb2fe4ce96d9c830282300025a444410
Luãn Costa <luanmarcoscosta@outlook.com>
2021-01-03 10:40:42 -0300

    Implementcoes e ajustes

---
+++ Modulo3/data.js
@@ -0,0 +1,40 @@
+module.exports = [{
+        id: "ifNSDGgBV94",
+        title: "Mayk Brito, Student Experience at Rocketseat | Alumni 09",
+        duration: "1:03:30 Horas",
+        price: "free",
+        featured: true
+    },
+    {
+        id: "1SPkgcrIf74",
+        title: "Será que é possível aprender JavaScript com Desafios? | Code/Drops #62",
+        duration: "14:32 Minutos",
+        price: "free"
+    },
+    {
+        id: "pbO3nnqyxlE",
+        title: "9 atitudes que nos fazem querer desistir (e como superar) | Code/Drops #63",
+        duration: "26:43 Minutos",
+        price: "free"
+    },
+    {
+        id: "7pqp3IKyC0s",
+        title: "7 Tags HTML que Você Precisa Conhecer | Code/Drops #58",
+        duration: "19:30 Minutos",
+        price: "free",
+        featured: true
+    },
+    {
+        id: "DsbeclaivTo",
+        title: "Como aprender CSS? LocalStorage, Site Responsivo, RefreshToken, GraphQL | #PR 17",
+        duration: "21:19 Minutos",
+        price: "free"
+    },
+    {
+        id: "RlK8nu_YErc",
+        title: "Design UX/UI, Home Office, Dev em Cidade Pequena, Portfólio, Insegurança | #PR 11",
+        duration: "13:58 Minutos",
+        price: "free",
+        featured: true
+    }
+]
--- Modulo3/server.js
+++ Modulo3/server.js
@@ -2,22 +2,52 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res) {
-    return res.render("about")
+
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/36084152?s=460&u=e440dc38fH4aYeFL7mbP3WEV4Y78X3TMRmAzpmRc&v=4",
+        name: "Luãn Costa",
+        role: "Estudante, Podcaster, Leitor e Escritor",
+        description: "Estudante de dev Web, tenho como Hobies produção de Podcasts e Escrever (esse ano meu livro sai) e ler é um amor que desenvolvi depois do meu 20º livro lido",
+        links: [
+            { name: "GitHub", url: "https://github.com/LuanMarcosCosta" },
+            { name: "Instagram", url: "https://www.instagram.com/luan_costa_21/" },
+            { name: "Twitter", url: "https://twitter.com/pensadormeiabok" }
+        ]
+    }
+
+    return res.render("about", { about: about })
 })
 
 
 server.get("/portfolio", function(req, res) {
-    return res.render("portfolio.njk")
+    return res.render("portfolio.njk", { items: videos })
+})
+
+server.get("/video", function(req, res) {
+    const id = req.query.id
+
+    const video = videos.find(function(video) {
+        video.id == id
+
+        if (!video) {
+            return res.send("Video Not Found")
+        }
+    })
+
+    return res.render("video", { item: video })
 })
 
 server.listen(5000, function() {
